f04f8a702f2c887afa47046a657aa6b20404f163
Peter Oliver <git@mavit.org.uk>
2025-11-25 14:05:11 +0000

    Add `node_modules/.bin/` to `exec-path` within this project only.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,7 @@
 ;; https://github.com/flycheck/flycheck/issues/1384#issuecomment-356919186
 ((js-mode
   . ((eval . (progn
-               (add-to-list 'exec-path
+               (add-to-list (make-local-variable 'exec-path)
                             (concat (locate-dominating-file default-directory
                                                             ".dir-locals.el")
                                     "node_modules/.bin/")))))))
