460dcb99ef13073e1551b10f036b51b1ed77cc4b
Peter Oliver <git@mavit.org.uk>
2025-11-25 14:33:53 +0000

    Tidy up `.dir-locals.el`.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,9 @@
-;; https://github.com/flycheck/flycheck/issues/1384#issuecomment-356919186
 ((js-mode
-  . ((eval . (progn
-               (add-to-list (make-local-variable 'exec-path)
-                            (concat (locate-dominating-file default-directory
-                                                            ".dir-locals.el")
-                                    "node_modules/.bin/")))))))
+  . ((eval
+      ;; Based on the discussion starting at
+      ;; https://github.com/flycheck/flycheck/issues/1384#issuecomment-356919186
+      ;; and https://emacs.stackexchange.com/a/53017/14429
+      . (setq-local flycheck-javascript-eslint-executable
+                    (expand-file-name "node_modules/.bin/eslint"
+                                      (locate-dominating-file default-directory
+                                                              dir-locals-file)))))))
